0f2759798c86b02a5a70d31164afa65124a355fa
GalangDhaffa <davagalang3456@gmail.com>
2023-12-21 12:00:22 +0700

    insya allah update terakhir

--- js/script.js
+++ js/script.js
@@ -1,8 +1,19 @@
-const menuToggle = document.querySelector('.menu-toggle input');
-const nav = document.querySelector('nav ul');
+const menuToggle = document.querySelector(".menu-toggle input");
+const nav = document.querySelector("nav ul");
 
-menuToggle.addEventListener('click', function () {
-    nav.classList.toggle('slide');
-})
+menuToggle.addEventListener("click", function () {
+  nav.classList.toggle("slide");
+});
 
-
+document.addEventListener("DOMContentLoaded", function () {
+    const navLinks = document.querySelectorAll("nav ul li a");
+    const mobileNav = document.querySelector("nav ul");
+    const checkbox = document.getElementById("nav-toggle");
+  
+    navLinks.forEach((link) => {
+      link.addEventListener("click", function () {
+        mobileNav.classList.remove("slide");
+        checkbox.checked = false;
+      });
+    });
+  });
