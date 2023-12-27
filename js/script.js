0ff5b93611c14a97db927aa513b336a6704c3bdb
GalangDhaffa <davagalang3456@gmail.com>
2023-12-27 15:59:05 +0700

    update

--- js/script.js
+++ js/script.js
@@ -6,14 +6,14 @@ menuToggle.addEventListener("click", function () {
 });
 
 document.addEventListener("DOMContentLoaded", function () {
-    const navLinks = document.querySelectorAll("nav ul li a");
-    const mobileNav = document.querySelector("nav ul");
-    const checkbox = document.getElementById("nav-toggle");
-  
-    navLinks.forEach((link) => {
-      link.addEventListener("click", function () {
-        mobileNav.classList.remove("slide");
-        checkbox.checked = false;
-      });
+  const navLinks = document.querySelectorAll("nav ul li a");
+  const mobileNav = document.querySelector("nav ul");
+  const checkbox = document.getElementById("nav-toggle");
+
+  navLinks.forEach((link) => {
+    link.addEventListener("click", function () {
+      mobileNav.classList.remove("slide");
+      checkbox.checked = false;
     });
   });
+});
